5f60c6884839f464b2ee92acdc4064ee3696b61d
liuwei <weiliu345@gmail.com>
2013-07-09 19:52:28 +0800

    删除WebApiUtils.java

--- src/main/java/com/lebo/service/account/ShiroApiRolesAuthorizationFilter.java
+++ src/main/java/com/lebo/service/account/ShiroApiRolesAuthorizationFilter.java
@@ -1,11 +1,13 @@
 package com.lebo.service.account;
 
+import com.lebo.rest.dto.ErrorDto;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authz.RolesAuthorizationFilter;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * 用户访问API，已登录但权限不足，返回JSON。
@@ -18,13 +20,17 @@ public class ShiroApiRolesAuthorizationFilter extends RolesAuthorizationFilter {
     @Override
     protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws IOException {
         Subject subject = getSubject(request, response);
+        PrintWriter out = response.getWriter();
+
         // If the subject isn't identified, return an unauthorized error
         if (subject.getPrincipal() == null) {
-            WebApiUtils.sendUnauthorized(request, response);
+            out.write(ErrorDto.UNAUTHORIZED.toJson());
         } else {
             // If subject is known but not authorized, return an forbidden error
-            WebApiUtils.sendForbidden(request, response);
+            out.write(ErrorDto.FORBIDDEN.toJson());
         }
+
+        out.flush();
         return false;
     }
 }
--- src/main/java/com/lebo/service/account/ShiroApiUserFilter.java
+++ src/main/java/com/lebo/service/account/ShiroApiUserFilter.java
@@ -1,9 +1,11 @@
 package com.lebo.service.account;
 
+import com.lebo.rest.dto.ErrorDto;
 import org.apache.shiro.web.filter.authc.UserFilter;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import java.io.PrintWriter;
 
 /**
  * 当需要客户端登录时，返回合适的JSON，而不是跳转到登录页面。
@@ -16,7 +18,9 @@ public class ShiroApiUserFilter extends UserFilter {
 
     @Override
     protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
-        WebApiUtils.sendUnauthorized(request, response);
+        PrintWriter out = response.getWriter();
+        out.write(ErrorDto.UNAUTHORIZED.toJson());
+        out.flush();
         return false;
     }
 }
