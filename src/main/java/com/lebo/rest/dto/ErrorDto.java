2bb543a2ee7fe0bc13e4edb087c7c8ccde1ef3e9
liuwei <weiliu345@gmail.com>
2013-07-12 10:27:56 +0800

    修改错误码

--- src/main/java/com/lebo/rest/dto/ErrorDto.java
+++ src/main/java/com/lebo/rest/dto/ErrorDto.java
@@ -3,22 +3,25 @@ package com.lebo.rest.dto;
 import org.springside.modules.mapper.JsonMapper;
 
 /**
- * 状态码命名规则：API版本号 + 3位数字，"3位数字"含义尽量与HTTP状态码对应。
- * 例如：API版本号v1，客户端请求错误，错误码为10400。其中"10"表示版本号1.0，400表示错误的请求（HTTP状态码400）
- * <p/>
+ * <p>
+ * ErrorDto状态码与HTTP状态码对应。
+ * 会根据需要增加HTTP没有的状态码。
+ * </p>
+ * <p>
  * 不要在此类外部设置状态码。
+ * </p>
  *
  * @author: Wei Liu
  * Date: 13-6-27
  * Time: PM6:14
  */
 public class ErrorDto {
-    public static final ErrorDto BAD_REQUEST = new ErrorDto("Bad Request", 10400);
-    public static final ErrorDto UNAUTHORIZED = new ErrorDto("Unauthorized", 10401);
-    public static final ErrorDto FORBIDDEN = new ErrorDto("Forbidden", 10403);
-    public static final ErrorDto NOT_FOUND = new ErrorDto("Not Found", 10404);
-    public static final ErrorDto DUPLICATE = new ErrorDto("Duplicate", 10427);
-    public static final ErrorDto INTERNAL_SERVER_ERROR = new ErrorDto("Internal Server Error", 10500);
+    public static final ErrorDto BAD_REQUEST = new ErrorDto("Bad Request", 400);
+    public static final ErrorDto UNAUTHORIZED = new ErrorDto("Unauthorized", 401);
+    public static final ErrorDto FORBIDDEN = new ErrorDto("Forbidden", 403);
+    public static final ErrorDto NOT_FOUND = new ErrorDto("Not Found", 404);
+    public static final ErrorDto DUPLICATE = new ErrorDto("Duplicate", 427);
+    public static final ErrorDto INTERNAL_SERVER_ERROR = new ErrorDto("Internal Server Error", 500);
 
     private static JsonMapper jsonMapper = JsonMapper.nonDefaultMapper();
     private String json;
@@ -30,11 +33,11 @@ public class ErrorDto {
     }
 
     public static ErrorDto newBadRequestError(String message) {
-        return new ErrorDto(message, 10400);
+        return new ErrorDto(message, 400);
     }
 
     public static ErrorDto newInternalServerError(String message) {
-        return new ErrorDto(message, 10500);
+        return new ErrorDto(message, 500);
     }
 
     private ErrorDto(String message, int code) {
