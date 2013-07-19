3932bd14a2cfd456e9e79fc269547840d11a6511
liuwei <weiliu345@gmail.com>
2013-07-19 12:12:09 +0800

    改进代码

--- src/main/java/com/lebo/service/FavoriteService.java
+++ src/main/java/com/lebo/service/FavoriteService.java
@@ -1,12 +1,10 @@
 package com.lebo.service;
 
-import com.lebo.entity.Comment;
 import com.lebo.entity.Favorite;
 import com.lebo.entity.Post;
 import com.lebo.repository.FavoriteDao;
 import com.lebo.rest.dto.StatusDto;
 import com.lebo.service.param.PaginationParam;
-import com.lebo.service.param.ShowCommentParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -31,7 +29,7 @@ public class FavoriteService extends AbstractMongoService {
         favoriteDao.save(new Favorite(userId, postId));
     }
 
-    public void destroy(String userId, String postId){
+    public void destroy(String userId, String postId) {
         Favorite favorite = favoriteDao.findByUserIdAndPostId(userId, postId);
         favoriteDao.delete(favorite);
     }
@@ -48,19 +46,13 @@ public class FavoriteService extends AbstractMongoService {
         return (int) mongoTemplate.count(new Query(new Criteria(Favorite.POST_ID_KEY).is(postId)), Favorite.class);
     }
 
-    public List<StatusDto> show(String userId, PaginationParam param){
+    public List<StatusDto> show(String userId, PaginationParam param) {
         List<Favorite> favorites = favoriteDao.findByUserId(userId, param);
         List<String> ids = new ArrayList<String>();
-        for(int i=0; i<favorites.size(); i++){
+        for (int i = 0; i < favorites.size(); i++) {
             ids.add(favorites.get(i).getPostId());
         }
-        // 获取Posts
         List<Post> posts = statusService.findPosts(ids);
-        // posts转为StatusDto
-        List<StatusDto> statusDtos = new ArrayList<StatusDto>();
-        for(int i=0; i<posts.size(); i++){
-            statusDtos.add(statusService.toStatusDto(posts.get(i)));
-        }
-        return statusDtos;
+        return statusService.toStatusDtoList(posts);
     }
 }
