e3189359f2d046a7eca2ba6e6a029e0a5c108d7c
liuwei <weiliu345@gmail.com>
2013-08-02 15:50:14 +0800

    添加jms

---
+++ src/main/java/com/lebo/jms/AdvancedNotifyMessageProducer.java
@@ -0,0 +1,55 @@
+package com.lebo.jms;
+
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+
+import javax.jms.*;
+
+/**
+ * JMS用户变更消息生产者.
+ * <p/>
+ * 使用jmsTemplate将用户变更消息分别发送到queue与topic.
+ *
+ * @author calvin
+ */
+public class AdvancedNotifyMessageProducer {
+
+    private JmsTemplate jmsTemplate;
+    private Destination notifyQueue;
+    private Destination notifyTopic;
+
+    /**
+     * 当用户被关注、喜欢、转播、at、评论、回复评论时，向用户发送通知。
+     *
+     * @param userId      该用户关注了一个用户
+     * @param followingId 该用户被关注了，会收到通知
+     */
+    //使用jmsTemplate的send/MessageCreator()发送Map类型的消息并在Message中附加属性用于消息过滤.
+    public void sendAppPushNotification(final String userId, final String screenName, final String followingId) {
+        jmsTemplate.send(notifyQueue, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+
+                MapMessage message = session.createMapMessage();
+                message.setString("screenName", screenName);
+                message.setString("followingId", followingId);
+
+                message.setStringProperty("objectType", "user");
+
+                return message;
+            }
+        });
+    }
+
+    public void setJmsTemplate(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+
+    public void setNotifyQueue(Destination notifyQueue) {
+        this.notifyQueue = notifyQueue;
+    }
+
+    public void setNotifyTopic(Destination nodifyTopic) {
+        this.notifyTopic = nodifyTopic;
+    }
+}
