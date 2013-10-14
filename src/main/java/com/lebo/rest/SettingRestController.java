07d356439949ea584576c794d440045fe684e745
liuwei <weiliu345@gmail.com>
2013-10-14 19:08:10 +0800

    推荐应用对ios和android客户端返回不同下载地址

--- src/main/java/com/lebo/rest/SettingRestController.java
+++ src/main/java/com/lebo/rest/SettingRestController.java
@@ -3,8 +3,10 @@ package com.lebo.rest;
 import com.lebo.entity.RecommendedApp;
 import com.lebo.entity.Setting;
 import com.lebo.service.SettingService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -37,8 +39,13 @@ public class SettingRestController {
 
     @RequestMapping(value = "recommendedApps", method = RequestMethod.GET)
     @ResponseBody
-    public Object recommendedApps() {
-        List<RecommendedApp> recommendedApps = settingService.getEnabledRecommendedApp();
-        return settingService.toRecommendedAppDtos(recommendedApps);
+    public Object recommendedApps(@RequestHeader("User-Agent") String userAgent) {
+        String type = SettingService.RECOMMENDED_APP_TYPE_IOS;
+        if (StringUtils.containsIgnoreCase(userAgent, "Android")) {
+            type = SettingService.RECOMMENDED_APP_TYPE_ANDROID;
+        }
+
+        List<RecommendedApp> recommendedApps = settingService.getEnabledRecommendedApp(type);
+        return settingService.toRecommendedAppDtos(recommendedApps, type);
     }
 }
