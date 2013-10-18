b55bc9f174f75e8167df14aa0685b8aaee51930a
liuwei <weiliu345@gmail.com>
2013-10-18 18:54:41 +0800

    添加删除评论功能

--- src/main/java/com/lebo/entity/Comment.java
+++ src/main/java/com/lebo/entity/Comment.java
@@ -18,6 +18,7 @@ import java.util.List;
 public class Comment extends IdEntity {
     @Indexed
     private String userId;
+    public static final String USER_ID_KEY = "userId";
     private Date createdAt;
     private String text;
     private boolean truncated;
--- src/main/java/com/lebo/service/param/CommentListParam.java
+++ src/main/java/com/lebo/service/param/CommentListParam.java
@@ -1,12 +1,10 @@
 package com.lebo.service.param;
 
-import org.hibernate.validator.constraints.NotBlank;
-
 public class CommentListParam extends PaginationParam {
     private String postId;
+    private String userId;
     private Boolean hasVideo;
 
-    @NotBlank
     public String getPostId() {
         return postId;
     }
@@ -21,5 +19,13 @@ public class CommentListParam extends PaginationParam {
 
     public void setHasVideo(Boolean hasVideo) {
         this.hasVideo = hasVideo;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 }
