161e9523a4281af6bc15f5694140277871080772
liuwei <weiliu345@gmail.com>
2013-11-12 16:53:08 +0800

    优化imports

--- src/main/java/com/lebo/rest/StatusRestController.java
+++ src/main/java/com/lebo/rest/StatusRestController.java
@@ -14,7 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
